6443bd3c68ca316e9c7036e8ce8736bd6d783132
Wasilewski <e-jbww@gft.com>
2025-01-03 10:10:56 +0100

    using notatnik_app_fe_static submodule for static content

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getAllNotes } from './requests.ts';
 import { NoteModel } from './Note.tsx';
+import { Logo } from 'notatnik_app_fe_static';
 
 const App = () => {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -16,29 +17,29 @@ const App = () => {
 
   return (
     <>
-      <img src={require('./favicon.png')} width='100px' alt='logo'/>
       <div className='wrapper'>
-      <div className='notes-wrapper'>
-        <div className='notes-list'>
-          {notes.length && notes.map(note => (
-            <span 
-              className={`note-title-list ${note.title === selectedNote?.title && 'selected-note-title'}`} 
-              onClick={() => setSelectedNote(note)}
-            >
-              {note.title}
-            </span>
-          ))}
-          {/* todo: infinite scroll */}
-        </div>
-        {selectedNote && (
-          <div className='note-wrapper'>
-            <span className='note-title-wrapper'>{selectedNote.title}</span>
-            <div className='break'/>
-            <div className='note-contents-wrapper'>{selectedNote.contents}</div>
+      <Logo />
+        <div className='notes-wrapper'>
+          <div className='notes-list'>
+            {notes.length && notes.map(note => (
+              <span 
+                className={`note-title-list ${note.title === selectedNote?.title && 'selected-note-title'}`} 
+                onClick={() => setSelectedNote(note)}
+              >
+                {note.title}
+              </span>
+            ))}
+            {/* todo: infinite scroll */}
           </div>
-        )}
+          {selectedNote && (
+            <div className='note-wrapper'>
+              <span className='note-title-wrapper'>{selectedNote.title}</span>
+              <div className='break'/>
+              <div className='note-contents-wrapper'>{selectedNote.contents}</div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 }
