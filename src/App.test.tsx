bf5b95eb99fb825b0f6e76a55f000e82ecb2cbe8
Wasilewski <e-jbww@gft.com>
2025-01-09 13:58:11 +0100

    updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,33 +1,65 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
-import axios from 'axios';
+import { ajax } from 'rxjs/ajax';
+import { Observable } from 'rxjs';
+import nock from 'nock';
+import { JSDOM } from 'jsdom';
 
-jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+// jest.mock('rxjs/ajax');
+// const mockedAjax = ajax as jest.Mocked<typeof ajax>;
+
+const { window } = new JSDOM('', { url: 'http://localhost:8080' });
 
 test('renders notes', async () => {
-  mockedAxios.get.mockResolvedValue({
-    data: [
-      {
-        title: 'first',
-        contents: ['one', 'two'],
-      },
-      {
-        title: 'second',
-        contents: ['three', 'four'],
-      },
-    ],
-  });
+  // mockedAjax.get.mockResolvedValue(Observable.of({
+  //   data: [
+  //     {
+  //       title: 'first',
+  //       contents: ['one', 'two'],
+  //     },
+  //     {
+  //       title: 'second',
+  //       contents: ['three', 'four'],
+  //     },
+  //   ]
+  // }));
+
+  nock('http://localhost:8080').defaultReplyHeaders({
+    'access-control-allow-origin': '*',
+    'access-control-allow-headers': 'x-requested-with',
+  }).options('/v1/paginated/notes?start=0&end=40').reply(200);
+  nock('http://localhost:8080').defaultReplyHeaders({
+    'access-control-allow-origin': '*',
+    'access-control-allow-headers': 'x-requested-with',
+  }).get('/v1/paginated/notes?start=0&end=40').reply(200, [
+    {
+      title: 'first',
+      contents: ['one', 'two'],
+    },
+    {
+      title: 'second',
+      contents: ['three', 'four'],
+    },
+  ]);
 
   render(<App />);
 
   await waitFor(() => {
-    expect(screen.getByText('first')).toBeInTheDocument();
-    expect(screen.getByText('second')).toBeInTheDocument();
+    const first = screen.getByText('first');
+    expect(first).toBeInTheDocument();
+    first.click();
+  });
 
+  await waitFor(() => {
     expect(screen.getByText('one')).toBeInTheDocument();
     expect(screen.getByText('two')).toBeInTheDocument();
+    const second = screen.getByText('second');
+    expect(second).toBeInTheDocument();
+    second.click();
+  });
+
+  await waitFor(() => {
     expect(screen.getByText('three')).toBeInTheDocument();
     expect(screen.getByText('four')).toBeInTheDocument();
   });
--- src/requests.ts
+++ src/requests.ts
@@ -5,9 +5,7 @@ import { Observable, map, catchError, of } from 'rxjs';
 export const getAllNotes: () => Observable<NoteModel[]> = () => {
   return ajax.get('http://localhost:8080/v1/notes').pipe(
     map((res) => res.response as NoteModel[]),
-    catchError((error) => {
-      return of(error);
-    })
+    catchError((error) => of(error))
   );
 };
 
@@ -17,8 +15,6 @@ export const getPaginatedNotes: (start: number, end: number) => Observable<NoteM
 ) => {
   return ajax.get(`http://localhost:8080/v1/paginated/notes?start=${start}&end=${end}`).pipe(
     map((res) => res.response as NoteModel[]),
-    catchError((error) => {
-      return of(error);
-    })
+    catchError((error) => of(error))
   );
 };
